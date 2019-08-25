6618fba3cc9d5e8116092a40d08c5d155c21fa91
Alexandre Leal <aleal4343@gmail.com>
2019-08-24 22:25:36 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const server = express();
+
+server.use(express.json());
+
+server.use((req, res, next) => {
+  console.log(`\x1b[35m${req.method} - ${req.url}\x1b[0m`);
+  return next();
+});
+//query params
+//Route params
+//body
+const users = ["Aleal", "Bleal", "Cleal"];
+
+function checkUserNamePresent(req, res, next) {
+  const { name } = req.body.name;
+  if (!name) {
+    return res.status(400).json({ error: "User name required!" });
+  }
+  req.user = name;
+  return next();
+}
+
+function checkUserExists(req, res, next) {
+  const user = users[req.params.index];
+  if (!user) {
+    return res.status(404).json({ error: "User does not exists!" });
+  }
+  req.user = user;
+  return next();
+}
+
+//all users
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+//an user
+server.get("/users/:index", checkUserExists, (req, res) => {
+  res.json(req.user);
+});
+//add user
+server.post("/users/", checkUserNamePresent, (req, res) => {
+  users.push(req.user);
+  res.json(users);
+});
+//update user
+server.put(
+  "/users/:index",
+  checkUserExists,
+  checkUserNamePresent,
+  (req, res) => {
+    console.log(req);
+    users[index] = req.user;
+    res.json(users[index]);
+  }
+);
+//delete user
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+  res.json(users.splice(index, 1));
+});
+
+server.listen(7777);
